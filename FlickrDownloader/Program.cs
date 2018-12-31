4d7db42aee70ed5debcc91fb33397cc14d403867
falcuun <falcuun@gmail.com>
2018-12-31 01:58:30 +0100

    Add project files.

---
+++ FlickrDownloader/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlickrDownloader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
