025a1c265227f52a96163e94c3a2bb47f3b1c6bd
falcuun <falcuun@gmail.com>
2018-12-31 19:34:12 +0100

    Added some Comments And desinsected some bugs

--- FlickrDownloader/FlickrConnection.cs
+++ FlickrDownloader/FlickrConnection.cs
@@ -9,42 +9,46 @@ namespace FlickrDownloader
 {
     class FlickrConnection
     {
+        /* Declaring Private Variables
+         */
         #region Private
-        private static readonly HttpClient client = new HttpClient();
-        private JSONObjectGenerator jog = new JSONObjectGenerator();
+        private static readonly HttpClient client = new HttpClient(); // HTTP Client for Establishing Connection
+        private JSONObjectGenerator jog = new JSONObjectGenerator(); // JSONObjectGenerator class Object
 
-        private const string BASE_URL = "https://api.flickr.com/services/rest/?method=";
-        private const int DEFAULT_IMAGE_NUMBER = 100;
-        private const int DEFAULT_PAGE_NUMBER = 1;
-        private int Images_Per_Page;
-        private int Number_Of_Pages;
+        private const string BASE_URL = "https://api.flickr.com/services/rest/?method="; // Base URL For Flickr APIs
+        private const int DEFAULT_IMAGE_NUMBER = 100; // Default Number Of Images shown per page
+        private const int DEFAULT_PAGE_NUMBER = 1; // Default Page
+        private int Images_Per_Page; // User Entered Number of Images per Page
+        private int Number_Of_Pages; // User Entered Number of Pages
 
         #region Flickr Format Request
         private const string JSONFormatRequest = "&format=json&nojsoncallback=1";
         #endregion
 
         #region Flickr Methods
-        private const string Galleries_GetPHotos = "flickr.galleries.getPhotos&api_key=";
-        private const string People_NSID = "flickr.urls.lookupUser&api_key=";
-        private const string GetPublicPhotos = "flickr.people.getPublicPhotos&api_key=";
-        private const string GetSizes = "flickr.photos.getSizes&api_key=";
+        private const string Galleries_GetPHotos = "flickr.galleries.getPhotos&api_key="; // Galleries GetPhotos 
+        private const string People_NSID = "flickr.urls.lookupUser&api_key="; // Urls LookUpUser 
+        private const string GetPublicPhotos = "flickr.people.getPublicPhotos&api_key="; // People GetPublicPhotos 
+        private const string GetSizes = "flickr.photos.getSizes&api_key="; // Photos GetSizes
         #endregion
-
-
-
-        #endregion
-
-
-
-        #region Public
-        public string API_KEY { get; set; }
-        public FlickrConnection() { }
-
+        #region Private Methods
+        /*
+            Method for getting the JSON Response String from the API URL 
+        */
         private static async Task<string> GetMessage(string url)
         {
-            var responseString = await client.GetStringAsync(url);
+            string responseString = await client.GetStringAsync(url);
             return responseString;
         }
+
+        #endregion
+        #endregion
+
+        #region Public
+        /* Declaring Public Variables
+         */
+        public string API_KEY { get; set; }
+        public FlickrConnection() { }
 
         #region Public Methods
         /*
@@ -66,6 +70,8 @@ namespace FlickrDownloader
         }
         /*
          Using flickr.urls.lookupUser& API Method to get User NSID
+         Calling GetUserNSID Methods From JSONObjectGenerator Class that takes a JSON Response string as Argument
+         Parses it, and returns the Flickr User NSID.
         */
         public string GenerateLookUpUserAPI(string User_URL)
         {
@@ -75,7 +81,9 @@ namespace FlickrDownloader
             NSID = jog.GetUserNSID(GetMessage(UserNSIDAPIUrl).Result);
             return NSID;
         }
-
+        /*
+         * For Now, takes in a desired number as an argument and checks if it's larger than 500 (Max images per page). 
+         */
         public bool Per_Page(string PerPage)
         {
             try
@@ -97,7 +105,9 @@ namespace FlickrDownloader
                 return false;
             }
         }
-
+        /*
+         * For Now, takes in a desired number as an argument and checks if it's smaller than 1 (Max images per page). 
+         */
         public bool Num_Pages(string PagesNumber)
         {
             try
@@ -119,21 +129,26 @@ namespace FlickrDownloader
                 return false;
             }
         }
-
+        /*
+         * Takes in a Photo_ID and forms a complete API Url for Flickr's GetSizes Method and returns it.
+         */
         public string GenerateGetSizesAPI(string Photo_ID)
         {
             string photo_id = "&photo_id=" + Photo_ID;
             string Photos_GetSizes = BASE_URL + GetSizes + API_KEY + photo_id + JSONFormatRequest;
             return Photos_GetSizes;
         }
-
+        /*
+         * Takes in Flickr User URL as an argument along with Location where the images should be saved and then it downloads the images one by one
+         * From the List of Photo Ids from JSON Object Generator Class And then Flushes the List for new IDs to be filled in from the next page.
+         */
         public void DownloadPublicPhotos(string User_URL, string Download_Location)
         {
             string UserID = "&user_id=" + GenerateLookUpUserAPI(User_URL);
-
             for (int i = 1; i <= Number_Of_Pages; i++)
             {
                 string PublicPhotosAPIUrl = BASE_URL + GetPublicPhotos + API_KEY + UserID + "&per_page=" + Images_Per_Page + "&page=" + i + "&format=json&nojsoncallback=1";
+                jog.User_Information(GetMessage(PublicPhotosAPIUrl).Result, Images_Per_Page);
                 string DL_Loc = Download_Location + "\\";
                 Console.WriteLine(PublicPhotosAPIUrl);
                 jog.GetPhotoId(GetMessage(PublicPhotosAPIUrl).Result);
@@ -141,14 +156,14 @@ namespace FlickrDownloader
                 {
                     using (WebClient client = new WebClient())
                     {
-                        foreach (string Photo_ID in jog.ListOfIDs)
+                        foreach (string Photo_ID in jog.ListOfPhotoIDs)
                         {
                             string DownloadLink = jog.GetSizeDownload(GetMessage(GenerateGetSizesAPI(Photo_ID)).Result);
                             Console.WriteLine(DownloadLink);
                             client.DownloadFile(DownloadLink, DL_Loc + Photo_ID + ".jpg");
                             Console.WriteLine(DL_Loc + Photo_ID + ".jpg");
                         }
-                        jog.EmptyListOfIDs();
+                        jog.FlushTheListOfIDs();
                     }
                 }
                 catch (Exception err)
--- FlickrDownloader/JSONObjectGenerator.cs
+++ FlickrDownloader/JSONObjectGenerator.cs
@@ -7,9 +7,14 @@ namespace FlickrDownloader
 {
     class JSONObjectGenerator
     {
-
-        public List<string> ListOfIDs = new List<string>();
-
+        /*
+         *List to be Filled with Photo IDs from JSON Response
+         */
+        public List<string> ListOfPhotoIDs = new List<string>();
+        /*
+         *  Method that takes in JSON String as an argument along with amount of images to download per page
+         *  Then prints out the information about USER that was searched for
+         */
         public void User_Information(string jsonValue, int per_page)
         {
             JSON UserInformation = JsonConvert.DeserializeObject<JSON>(jsonValue);
@@ -17,6 +22,10 @@ namespace FlickrDownloader
             if (UserInformation.photos.total > per_page)
             {
                 int Available_Pages = UserInformation.photos.total / per_page;
+                if (Available_Pages == 0)
+                {
+                    Available_Pages = 1;
+                }
                 Console.WriteLine("With " + per_page + " Image(s) per page, User has: " + Available_Pages + " Page(s) Available");
             }
             else
@@ -24,8 +33,10 @@ namespace FlickrDownloader
                 Console.WriteLine("With " + per_page + " images per page, User has: " + 1 + " Page(s) Available");
             }
         }
-
-
+        /*
+         *  Method that takes in JSON String as an Argument and then parses it to return the ID Of the Image
+         *  And add it to the List Of Photo IDs
+         */
         public void GetPhotoId(string jsonValue)
         {
             JSON PhotoIDs = JsonConvert.DeserializeObject<JSON>(jsonValue);
@@ -34,7 +45,7 @@ namespace FlickrDownloader
             {
                 foreach (PhotoModel model in PhotoIDs.photos.photo)
                 {
-                    ListOfIDs.Add(model.id);
+                    ListOfPhotoIDs.Add(model.id);
                 }
             }
             catch (NullReferenceException nre)
@@ -42,13 +53,20 @@ namespace FlickrDownloader
                 Console.WriteLine(nre.Message + " --GETPHOTOID");
             }
         }
-
+        /*
+         * Method that takes in JSON String as an argument and parses it to return
+         * The User NSID 
+         */
         public string GetUserNSID(string message)
         {
             JSON UserIDs = JsonConvert.DeserializeObject<JSON>(message);
             return UserIDs.user.id; 
         }
-
+        /*
+         * Method That Takes in JSON String as an Argument and Parses it to iterate through a list of sizes returned by API call
+         * When the method finds the Original image (With Original size) it returns the link to it (From JSON Response)
+         * And adds the _d.jpg to it for downloading purposes.
+         */
         public string GetSizeDownload(string message)
         {
             string jsonValue = message;
@@ -72,10 +90,12 @@ namespace FlickrDownloader
 
             return Download_Link;
         }
-
-        public void EmptyListOfIDs()
+        /*
+         * Method that Flushes the List of Photo IDs so that list would be empty
+         */
+        public void FlushTheListOfIDs()
         {
-            ListOfIDs.RemoveRange(0, ListOfIDs.Count);
+            ListOfPhotoIDs.RemoveRange(0, ListOfPhotoIDs.Count);
         }
     }
 }
