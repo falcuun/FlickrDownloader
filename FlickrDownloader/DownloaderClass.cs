31bac21180fc28de45d1bb5b17a99ac1ad500c01
Falcuun <falcuun@gmail.com>
2019-07-21 20:56:39 +0200

    Added a message telling user when the download is complete.

---
+++ FlickrDownloader/DownloaderClass.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Net;
+
+namespace FlickrDownloader
+{
+    class DownloaderClass
+    {
+        private const string GetSizes = "flickr.photos.getSizes&api_key=";
+        private const string BASE_URL = "https://api.flickr.com/services/rest/?method=";
+        private const string JSONFormatRequest = "&format=json&nojsoncallback=1";
+
+        public static string GenerateGetSizesAPI(string Photo_ID)
+        {
+            string photo_id = "&photo_id=" + Photo_ID;
+            string Photos_GetSizes = BASE_URL + GetSizes + Program.API_KEY + photo_id + JSONFormatRequest;
+            return Photos_GetSizes;
+        }
+
+        public static void DownloadImage(string URL, string file_path)
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
+                {
+                    client.DownloadFileAsync(new Uri(URL), file_path);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.InnerException);
+            }
+        }
+    }
+}
---
+++ FlickrDownloader/TextSearchPhotosDownload.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlickrDownloader
+{
+    class TextSearchPhotosDownload
+    {
+        private const string BASE_URL = "https://api.flickr.com/services/rest/?method="; // Base URL For Flickr APIs
+        private const string JSONFormatRequest = "&format=json&nojsoncallback=1";
+        private const string Search_By_Text = "flickr.photos.search&api_key=";
+        private int Images_Per_Page; // User Entered Number of Images per Page
+        private int Number_Of_Pages; // User Entered Number of Pages
+
+        JSONObjectGenerator jog = new JSONObjectGenerator();
+
+        public bool Per_Page(string PerPage)
+        {
+            try
+            {
+                int num = int.Parse(PerPage);
+                if (num > 500)
+                {
+                    Images_Per_Page = 500;
+                }
+                else
+                {
+                    Images_Per_Page = num;
+                }
+                return true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Per Page Number is not correct. Try again");
+                return false;
+            }
+        }
+
+        public bool Num_Pages(string PagesNumber)
+        {
+            try
+            {
+                int num = int.Parse(PagesNumber);
+                if (num <= 1)
+                {
+                    Number_Of_Pages = 1;
+                }
+                else
+                {
+                    Number_Of_Pages = num;
+                }
+                return true;
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Number of Pages is not correct. Try again");
+                return false;
+            }
+        }
+
+        private string GenerateTextSearchURL(string query, int page_index)
+        {
+            string url = BASE_URL + Search_By_Text + Program.API_KEY + "&text=" + query + "&sort=relevance" + "&per_page=" + Images_Per_Page + "&page=" + page_index + JSONFormatRequest;
+            return url;
+        }
+        public void TotalImages(string query)
+        {
+            jog.Query_Info(jog.GetMessage(GenerateTextSearchURL(query, 0)).Result);
+        }
+
+        private string FormDownloadPath(string path, string photo_id)
+        {
+            return path + "\\" + photo_id + ".jpg";
+        }
+
+        public void ObtainSearchedPhotos(string path, string query)
+        {
+            for (int i = 1; i <= Number_Of_Pages; i++)
+            {
+                jog.PopulateListOfPhotoIDs(jog.GetMessage(GenerateTextSearchURL(query, i)).Result);
+                foreach (string Photo_ID in jog.ListOfPhotoIDs)
+                {
+                    string DownloadLink = jog.GetSizeDownload(jog.GetMessage(DownloaderClass.GenerateGetSizesAPI(Photo_ID)).Result);
+                    DownloaderClass.DownloadImage(DownloadLink, FormDownloadPath(path, Photo_ID));
+                }
+                jog.FlushTheListOfIDs();
+            }
+            Console.WriteLine("Download Complete");
+        }
+
+
+    }
+}
